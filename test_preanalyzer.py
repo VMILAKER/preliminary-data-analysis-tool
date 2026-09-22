dbffb638150e92778edbf111881ada6bd974de26
dkotlyachkov <d_kotlyachkov@mail.ru>
2026-09-22 07:37:33 +0300

    Change filepath for test file

--- test_preanalyzer.py
+++ test_preanalyzer.py
@@ -4,6 +4,6 @@ import os
 from __init__ import PreAnalyzer
 
 if __name__ == '__main__':
-    filepath_test = os.path.join('my_file (1).csv')
+    filepath_test = os.path.join('test.csv')
     preanalyzer = PreAnalyzer(filepath_test)
     preanalyzer.gain_report()
